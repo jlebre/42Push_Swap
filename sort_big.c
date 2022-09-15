bbc8366335df516d28501c59af14e244709adf18
José Maria Lebre <94384240+jlebre@users.noreply.github.com>
2022-09-15 20:26:31 +0100

    Update

--- sort_big.c
+++ sort_big.c
@@ -6,7 +6,7 @@
 /*   By: jlebre <jlebre@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/08 17:21:23 by jlebre            #+#    #+#             */
-/*   Updated: 2022/09/15 20:14:28 by jlebre           ###   ########.fr       */
+/*   Updated: 2022/09/15 20:21:05 by jlebre           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,9 +28,9 @@ int	size_of_stack(void)
 	return (size + 1);
 }
 
-void	sort_big_1(int i, int j, int bit)
+void	sort_big_1(int i, int j, int bit, int size)
 {
-	while (j < size_of_stack())
+	while (j < size)
 	{
 		if (stack_a()->next->nb >> bit & 1)
 			ra();
@@ -52,14 +52,16 @@ void	sort_big(void)
 {
 	int		bit;
 	int		max_bits;
+	int		size;
 
 	bit = 0;
 	max_bits = 1;
-	while (size_of_stack() >> max_bits != 0)
+	size = size_of_stack();
+	while (size >> max_bits != 0)
 		max_bits++;
 	while (bit < max_bits)
 	{
-		sort_big_1(0, 0, bit);
+		sort_big_1(0, 0, bit, size);
 		bit++;
 	}
 }
